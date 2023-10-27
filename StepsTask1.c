77a8c0aa6d4a79b9279aca30548b8c62b14af146
Archie James Kershaw <146597047+kershw@users.noreply.github.com>
2023-10-27 13:17:13 +0000

    saving - read from file into array

---
+++ StepsTask1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Define any additional variables here
+
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+}
+
+// Complete the main function
+int main() {
+    char filename [21] = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+    
+    if (file == NULL) {
+        perror("There is no file, couldn't find data.");
+        return 1;
+    }
+
+    char data[20];
+    int count = 0;
+    char fitnessArray[60];
+    while (fgets(data, 60, file)) {
+        printf("%s\n", data);
+        fitnessArray[count] = *data;
+        count++;
+    }
+
+    fclose(file);
+}
