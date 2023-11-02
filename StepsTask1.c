00dbfec85ee7a1334ca832659a0480f85315f434
Archie James Kershaw <146597047+kershw@users.noreply.github.com>
2023-11-02 21:18:04 +0000

    completed task 1 yippee

--- StepsTask1.c
+++ StepsTask1.c
@@ -53,26 +53,31 @@ int main() {
 
     char temp[25];
     int fileLength = 0;
-    while (fgets(temp, 22, file))  { //finds the length of the file - for use with unseen files which may have varying lengths
+    while (fgets(temp, 30, file))  { //finds the length of the file - for use with unseen files which may have varying lengths
         fileLength++;
     }
 
     rewind(file); //puts the file pointer back to 0 so fgets() can be run again
     FITNESS_DATA STEP_INTERVAL [fileLength]; //defines the array of structs
     //defining various variables to be used within the while loop
-    char data[22]; 
+    char data[25]; 
     int count = 0; 
     //date length 11, time length 6, steps length 10; remember this or string formatting goes weird
     char OutputDate[11]; 
     char OutputTime[6];
     char OutputSteps[10];
 
-    while (fgets(data, 22, file)) { //buffer = length of max string + 1
+    while (fgets(data, 30, file)) { //buffer = a few characters more than max length for safety
         tokeniseRecord(data, ",", OutputDate, OutputTime, OutputSteps); //runs tokenise record function to get data from each line
         strcpy(STEP_INTERVAL[count].date, OutputDate); //copies date to struct
         strcpy(STEP_INTERVAL[count].time, OutputTime); //copies time to struct
         STEP_INTERVAL[count].steps = atoi(OutputSteps); //copies steps to struct - use atoi to save as int
         count++;
+    }
+
+    printf("Number of records in file: %d\n", fileLength);
+    for (int i=0; i<3; i++) {
+        printf("%s/%s/%d\n", STEP_INTERVAL[i].date, STEP_INTERVAL[i].time, STEP_INTERVAL[i].steps);
     }
 
     /* iterating through struct array for debugging
