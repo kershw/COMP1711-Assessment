79895399b8422911837fa83ac3a71e240ce1140b
Archie James Kershaw <146597047+kershw@users.noreply.github.com>
2023-11-14 16:46:04 +0000

    task 2, calc long continuous period + print it all in switch

--- StepsTask1.c
+++ StepsTask1.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// Define an appropriate struct
-typedef struct {
-	char date[11];
-	char time[6];
-	int steps;
-} FITNESS_DATA;
+#include "FitnessDataStruct.h"
 
 // Define any additional variables here
-
-
+FITNESS_DATA STEP_INTERVAL [1000]; //defines the array of structs
+char filename[30];
+int fileLength = 0;
 
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
@@ -40,25 +35,20 @@ void tokeniseRecord(const char *input, const char *delimiter,
     free(inputCopy);
 
 }
-
-// Complete the main function
-int main() {
-    char filename [21] = "FitnessData_2023.csv"; //gets file
+int getDataFromFile(char filename[30]) {
     FILE *file = fopen(filename, "r"); //opens file
     
     if (file == NULL) { //checks for no file error
-        perror("There is no file, couldn't find data.");
+        perror("Error, couldn't open the file.");
         return 1;
     }
 
     char temp[25];
-    int fileLength = 0;
     while (fgets(temp, 30, file))  { //finds the length of the file - for use with unseen files which may have varying lengths
         fileLength++;
     }
 
     rewind(file); //puts the file pointer back to 0 so fgets() can be run again
-    FITNESS_DATA STEP_INTERVAL [fileLength]; //defines the array of structs
     //defining various variables to be used within the while loop
     char data[25]; 
     int count = 0; 
@@ -75,17 +65,77 @@ int main() {
         count++;
     }
 
-    printf("Number of records in file: %d\n", fileLength);
-    for (int i=0; i<3; i++) {
-        printf("%s/%s/%d\n", STEP_INTERVAL[i].date, STEP_INTERVAL[i].time, STEP_INTERVAL[i].steps);
-    }
-
-    /* iterating through struct array for debugging
-    for (int i=0; i<fileLength; i++){
-        printf("%d\n", STEP_INTERVAL[i].steps);
-    }
-    */
-
-
     fclose(file);
+    return fileLength;
+
+}
+
+
+int calculations() { //in this function, calculate all the required values that the user may ask for (already have the file length though)
+    char FewestStepsArray [2][11]; //index 0 = date, index 1 = time
+    int FewestSteps = 1000000;
+    char LargestStepsArray [2][11]; //index 0 = date, index 1 = time
+    int LargestSteps = 0;
+    int Sum = 0;
+    char PeriodStart [2][11]; //index 0 = date, index 1 = time
+    char PeriodEnd [2][11]; //index 0 = date, index 1 = time
+    int Count = 0;
+    int LongestPeriod = 0;
+    
+    //iterate through all data
+    for (int i = 0; i < fileLength; i++) {
+        //find fewest steps
+        if (STEP_INTERVAL[i].steps < FewestSteps) {
+            FewestSteps = STEP_INTERVAL[i].steps;
+            strcpy(FewestStepsArray[0], STEP_INTERVAL[i].date);
+            strcpy(FewestStepsArray[1], STEP_INTERVAL[i].time);
+        }
+        //find largest steps
+        if (STEP_INTERVAL[i].steps > LargestSteps) {
+            LargestSteps = STEP_INTERVAL[i].steps;
+            strcpy(LargestStepsArray[0], STEP_INTERVAL[i].date);
+            strcpy(LargestStepsArray[1], STEP_INTERVAL[i].time);
+        }
+        //calculate the sum of the values in order to calculate the mean
+        Sum += STEP_INTERVAL[i].steps;
+        //find the longest continuous period where step count > 500
+        if (STEP_INTERVAL[i].steps > 500) {
+            
+        }
+    }
+    int mean = Sum / fileLength;
+
+    printf("\n%d\n", mean);
+    printf("%s\n%s\n", LargestStepsArray[0], LargestStepsArray[1]);
+
+    return 0;
+}
+// Complete the main function
+int main() {
+    char choice;
+    while (choice != 'Q') {
+        printf("Select from the following options:\n");
+        printf("A: Specify a filename to be imported\nB: Display the total number of records in the file\nC: Return the date and time of the timeslot with the fewest steps\n");
+        printf("D: Return the date and time of the timeslot with the largest number of steps\nE: Calculate the mean step count of all the records in the file");
+        printf("F: Find the longest continuous period where the step count is above 500 steps\nQ: Quit\n");
+
+        scanf(" %c", &choice);
+        switch (choice) {
+            case 'A':
+                printf("Input filename:\n");
+                scanf(" %s", filename);
+                int fileLength = getDataFromFile(filename);
+                
+                calculations();
+                break;
+
+            case 'B':
+                printf("Total records: %d\n", fileLength);
+                break;
+
+            case 'C':
+                break;
+        }
+    };
+
 }
